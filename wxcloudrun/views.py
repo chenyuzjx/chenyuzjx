dff133d423c6236cd0d97de180d964be79dc10fe
chenyuzjx <117984208+chenyuzjx@users.noreply.github.com>
2023-07-29 13:46:17 +0800

    Update views.py

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -7,7 +7,7 @@ from wxcloudrun.models import Counters
 
 
 logger = logging.getLogger('log')
-def hello_world(request):
+def hello_world(request, _):
     return '周军兴的第一个部署!\n'
 '''
 def index(request, _):
